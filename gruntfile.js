110cc0d6f471a4ec2c4325f37d57054dd0ebd2d4
Rohan Chambers <rohanchambers888@gmail.com>
2016-04-08 19:08:15 +0200

    Great start for project using Grunt and Bower

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,97 @@
 module.exports = function(grunt){
 
 	// Load all of the tasks automatically using the matchdep dependency
-	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+	//require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		
-		htmllint: {
-			options: {},
-			src: [
-			  '/*.html'
-			],
-		}
-    });
+		// SASS task config
+		sass: {
+			dist: {
+				files: {
+					'assets/css/styles.css' : 'assets/sass/styles.scss'
+				}
+			}
+		},
+		
+		// CSS min styles.css 
+		cssmin : {
+			minify : {
+				expand : true,
+				cwd : 'assets/css/',
+				src : [ '*.css', '!*.min.css' ],
+				dest : 'assets/css/',
+				ext : '.min.css'
+			}
+		},
 
-    grunt.registerTask('default', []);
+		// JShint for scripts
+        jshint: {
+            all: {
+                src: 'assets/js/scripts.js',
+	            options: {
+					camelcase: true,
+					noempty: true,
+					quotmark: 'single',
+				    curly: true,
+				    eqeqeq: true,
+				    eqnull: true,
+				    browser: true,
+				    globals: {
+				      jQuery: true
+				    }
+				}
+            }
+        },
+
+        // Minify JS
+        uglify: {
+            all: {
+                files: {
+                    'assets/js/scripts.min.js': 'assets/js/scripts.js'
+                }
+            }
+        },
+
+		// Using the BrowserSync Server for your static .html files.
+		browserSync: {
+		  default_options: {
+		    bsFiles: {
+		      src: [
+		        "assets/css/*.css",
+		        "*.html"
+		      ]
+		    },
+		    options: {
+		      watchTask: true,
+				proxy: "sdd.dev"
+		    }
+		  }
+		},
+
+ 		// Watch task config
+		watch: {
+			css: {
+				files: 'assets/sass/*.scss',
+				tasks: ['sass', 'cssmin']
+			},
+
+            javaScript: {
+                files: 'assets/js/**.js',
+                tasks: 'jshint'
+            }
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');	
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');	
+
+	grunt.registerTask('default', ['browserSync', 'watch', 'uglify']);
 
 };
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Slam Dunk Digital - London Design &amp; Development Agency</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
+        <link rel="stylesheet" href="assets/css/styles.min.css">
+        <script type="text/javascript" src="assets/js/vendors/modernizr-custom.js"></script>
+    </head>
+    <body>
+        <button>setup</button>
+
+
+        <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/scripts.min.js"></script>
+    </body>
+</html>
