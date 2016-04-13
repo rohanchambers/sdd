1ace15c5b083895d002d7dbf96074c289410a6d8
Rohan Chambers <rohanchambers888@gmail.com>
2016-04-13 18:42:25 +0200

    full page nav complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
-	console.log('Ready!');
-	var rohan = 'hello';
-	function tess() {
-		console.log(rohan);
+	// Defining a function to set size for #hero 
+	function fullscreen(){
+	    $('#home').css({
+	        width: jQuery(window).width(),
+	        height: jQuery(window).height()
+	    });
 	}
-	tess();
+
+	fullscreen();
+
+	// Run the function in case of window resize
+	$(window).resize(function() {
+	   fullscreen();         
+	});
+
+    // Bind a click event to anything with the class "toggle-nav"
+    $('.hamburger').click(function() {  
+          // Toggle the Body Class "show-nav"
+          $('body').toggleClass('show-nav');
+          $('#nav-icon').toggleClass('open');
+          // Deactivate the default behavior of going to the next page on click 
+          return false;
+    });   
+
+});
+
+// Toggle with hitting of ESC
+$(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+        $('body').toggleClass('show-nav');
+        $('#nav-icon').toggleClass('open');
+    }
 });
--- index.php
+++ index.php
@@ -6,13 +6,75 @@
         <title>Slam Dunk Digital - London Design &amp; Development Agency</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
-        <link rel="stylesheet" href="assets/css/styles.min.css">
-        <script type="text/javascript" src="assets/js/vendors/modernizr-custom.js"></script>
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <script type="text/javascript" src="assets/js/vendor/modernizr-full.js"></script>
     </head>
-    <body> 
+    <body>
 
+        <button class="hamburger" id="trigger-overlay" type="button">
+            <div id="nav-icon">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+        </button>
+        <div class="overlay overlay-hugeinc">
+            <header>
+                <h1>Slam Dunk Digital</h1>
+            </header>        
+            <nav role='navigation'>
+                <ul>
+                    <li><a href="#"><span>H</span>ome</a></li>
+                    <li><a href="#"><span class="yellow">W</span>hat we do</a></li>
+                    <li><a href="#"><span class="red">O</span>ur work</a></li>
+                    <li><a href="#"><span class="green">C</span>lients</a></li>
+                    <li><a href="#"><span class="blue">C</span>ontact</a></li>
+                    <li id="social">
+                        <i class="fa fa-facebook fa-1x"><a href="#"></a></i>
+                        <i class="fa fa-twitter fa-1x"><a href="#"></a></i>
+                        <i class="fa fa-instagram fa-1x"><a href="#"></a></i>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+                
+        <div id="bg-home">
+            <section id="home">
+
+                <header>
+                    <h1>Slam Dunk Digital</h1>
+                </header>
+
+                <div id="slogan">
+                    <span>We stand for</span>                    
+                    <p>
+                        Flair <span class="yellow">/</span> Motion <span class="red">/</span> <br>
+                        Elegance <span class="green">/</span><br>
+                        Perfection <span class="blue">/</span><br>
+                        10 out of 10 <span class="white">/</span><br>
+                    </p>
+                    <span id="all-the-time">All the time!</span>
+                </div><!-- End of Slogan -->
+
+            </section><!-- End of home -->
+
+        </div>
+
+        <div id="slide-services">
+            <section id="services">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </section><!-- End of home-->
+        </div>
 
         <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
-        <script type="text/javascript" src="assets/js/scripts.min.js"></script>
+        <script type="text/javascript" src="assets/js/vendor/classie.js"></script>
+        <script type="text/javascript" src="assets/js/vendor/nav-overlay.js"></script>
+        <script type="text/javascript" src="assets/js/scripts.js"></script>
     </body>
 </html>
