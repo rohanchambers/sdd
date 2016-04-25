8e74649014b76a4765db24827a4e41aee02da551
Rohan Chambers <rohanchambers888@gmail.com>
2016-04-25 21:08:46 +0200

    social sharing code

--- index.php
+++ index.php
@@ -4,18 +4,55 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Slam Dunk Digital - London Design &amp; Development Agency</title>
+        <meta name="title" content="We stand for FLAIR / MOTION / CREATIVITY / &amp; STYLE / 10 OUT OF 10 / Every time!">
+        <meta name="author" content="Slam Dunk Digital">
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
         <link rel="stylesheet" href="assets/css/styles.css">
         <script type="text/javascript" src="assets/js/vendor/modernizr-full.js"></script>
+
+        <!-- Open graph tags -->
+        <meta property="og:image"           content="http://slamdunkdigital.co.uk/" />
+        <meta property="og:title"           content="Welcome to Slam Dunk Digital" />
+        <meta property="og:description"     content="We stand for FLAIR / MOTION / CREATIVITY / &amp; STYLE / 10 OUT OF 10 / Every time!" />
+        <meta property="og:url"             content="http://slamdunkdigital.co.uk/" />    
+        <meta property="og:image:width"     content="600" />
+        <meta property="og:image:height"    content="600" />
+        <meta property="og:image:type"      content="image/jpg" /> 
+        <meta property="fb:app_id"          content="628421490629466" />
+
+        <meta name="twitter:card"           content="summary">
+        <meta name="twitter:site"           content="@slamdunkdigital">
+        <meta name="twitter:title"          content="Welcome to slamdunkdigital">
+        <meta name="twitter:description"    content="We stand for FLAIR / MOTION / CREATIVITY / &amp; STYLE / 10 OUT OF 10 / Every time!">
+        <meta name="twitter:creator"        content="@slamdunkdigital">
+        <meta name="twitter:image:src"      content="http://slamdunkdigital.co.uk/">
+        <meta name="twitter:domain"         content="slamdunkdigital.co.uk/">
     </head>
     <body>
-
         <div id="bg-home">
             <section id="home">
-                <header>
+                <header id="logo">
                     <h1>Slam Dunk Digital</h1>
                 </header>
+                <nav id="social-links" class="nav-global">
+                    <ul>
+                        <li><a href="https://www.facebook.com/slamdunkdigital" title="Facebook share" target="_blank"><i class="fa fa-facebook fa-1x btn-animate"></i></a></li>
+                        <li><a href="https://twitter.com/slamdunkdigital" title="Twitter share" target="_blank"><i class="fa fa-twitter fa-1x btn-animate"></i></a></li>
+                        <li><a href="https://www.instagram.com/slamdunkdigital" title="Instagram share" target="_blank"><i class="fa fa-instagram fa-1x btn-animate"></i></a></li>
+                        <li><a href="#nogo" id="share-me-cta" class="animatedClick fast" title="Share this site" data-target="share-me-panel"><i class="fa fa-1x fa-share-alt btn-animate"></i></a>
+                            <ul id="share-me" class="animated fadeInDown fast share-me-panel">
+                                <li><div class="fb-share-button" data-href="http://slamdunkdigital.co.uk/" data-layout="button_count"></div></li>
+                                <li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://slamdunkdigital.co.uk/" data-text="We stand for FLAIR / MOTION / CREATIVITY / &amp; STYLE / 10 OUT OF 10 / Every time!" data-via="slamdunkdigital">Tweet</a></li>                            
+                                <li><script type="IN/Share" data-url="http://slamdunkdigital.co.uk/"></script></li>
+                                <li><div class="g-plusone" data-annotation="none"></div></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
                 <div id="slogan">
                     <canvas id="demo-canvas"></canvas>
                     <span>We stand for</span>                    
@@ -40,12 +77,10 @@
             <section id="what-we-do">
                 <div class="col col-skew">
                     <header>
-                        <h2>What we do</h2>
+                        <h1>What we do</h1>
                         <p class="intro">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     </header>
-
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 
@@ -58,13 +93,13 @@
 
         <section id="work">
             <header>
-                <h2>Our work</h2>
+                <h1>Our work</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </header>
 
             <div class="row">
                 <div class="view view-first">  
-                    <img src="assets/img/work/phoneResq.jpg" alt="Phone Resq." /> 
+                    <img src="assets/img/work/slamdunkdigital.jpg" alt="Phone Resq." /> 
                     <div class="mask">
                         <h2>Phone ResQ.</h2>
                         <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
@@ -147,7 +182,7 @@
 
         <section id="contact">
             <header>
-                <h2>Contact</h2>
+                <h1>Contact</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </header>
         </section>
@@ -163,6 +198,7 @@
                 N1 0YL, UK</p>
                 <p><a href="tel:+4407917044058"><i class="fa fa-phone fa-lg"></i>+44 (0)79 17044058</a></p>
             </div>
+
             <div id="get-in-touch">
                 <h2>Get in touch</h2>
                 <p>
@@ -171,6 +207,7 @@
                     <a href="mailto:careers@slamdunkdigital.com">careers@slamdunkdigital.com</a>
                 </p>
             </div>
+
             <div id="follow-us">
                 <h2>Follow us on</h2>
                 <ul>
@@ -228,15 +265,15 @@
 
         <div class="overlay overlay-hugeinc">
             <header>
-                <h2>Slam Dunk Digital</h2>
+                <h1>Slam Dunk Digital</h1>
             </header>        
             <nav role='navigation'>
                 <ul>
-                    <li class="#home"><a href="#"><span>H</span>ome</a></li>
+                    <li class="home"><a href="#"><span class="letter-yellow">H</span><span class="letter-red">o</span><span class="letter-green">m</span><span class="letter-blue">e</span></a></li>
                     <li class="yellow"><a href="#what-we-do"><span class="yellow">W</span>hat we do</a></li>
                     <li class="red"><a href="#work"><span class="red">O</span>ur work</a></li>
                     <li class="green"><a href="#clients"><span class="green">C</span>lients</a></li>
-                    <li class="blue"><a href="#"contact><span class="blue">C</span>ontact</a></li>
+                    <li class="blue"><a href="#contact"><span class="blue">C</span>ontact</a></li>
                     <li id="social">
                         <i class="fa fa-facebook fa-1x"><a href="#"></a></i>
                         <i class="fa fa-twitter fa-1x"><a href="#"></a></i>
@@ -253,8 +290,27 @@
         <script src="assets/js/vendor/rAF.js"></script>
         <script src="assets/js/vendor/emitter.js"></script>
         <script src="assets/js/vendor/proximity.js"></script>
+        <script src="assets/js/vendor/css3-animate-it.js"></script>
         <!-- Google Maps api -->
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAcjk6nT2G48ymz2arA9YSIt-iGOKkUMfk&callback=initMap" async defer></script>
         <script src="assets/js/scripts.js"></script>
+        
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=168519230210632";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));   
+        </script>  
+        <!-- Twitter share -->
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        <!-- Google + share -->
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <!-- Linkedin Share -->
+        <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+        <!-- Google Maps api -->        
     </body>
 </html>
