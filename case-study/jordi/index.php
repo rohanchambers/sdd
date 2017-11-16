cbee665bc96ebfc82c5fa0d9b81452cbb6bf58fe
Rohan Chambers <rohanchambers888@gmail.com>
2017-11-16 12:17:34 +0000

    CS updated back and next links

--- case-study/jordi/index.php
+++ case-study/jordi/index.php
@@ -65,7 +65,7 @@ $PRODUCTION = true;
             <section id="case-study-nav">
                 <div class="col"><a href="../tesco/" class="go-prev" title="Previous Project"><i class="fa fa-2x fa-chevron-circle-left" aria-hidden="true"></i></a></div>
                 <div class="col"><a href="/#work" class="go-all" title="All Projects"><i class="fa fa-th fa-2x" aria-hidden="true"></i></a></div>
-                <div class="col"><a href="../perfect-peaks-brews/" class="go-next" title="Next Project"><i class="fa fa-2x fa-chevron-circle-right" aria-hidden="true"></i></a></div>
+                <div class="col"><a href="../phone-resq/" class="go-next" title="Next Project"><i class="fa fa-2x fa-chevron-circle-right" aria-hidden="true"></i></a></div>
             </section>
 
             <section id="case-study">
