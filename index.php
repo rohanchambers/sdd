300bcc365bb55e6880880ca94d5d02a9e24c42a7
Rohan Chambers <rohanchambers888@gmail.com>
2019-10-02 16:36:59 +0100

    Addition of social media links in the main header near nav

--- index.php
+++ index.php
@@ -67,13 +67,18 @@
                 </header>
                 <nav id="social-links" class="nav-global">
                     <ul>
-                        <li><span class="share-text">Share</span> <a href="#nogo" id="share-me-cta" class="animatedClick fast" title="Share this site" data-target="share-me-panel"><i class="fa fa-1x fa-share-alt btn-animate"></i></a>
+                        <li><span class="share-text">Share</span> <a href="#nogo" id="share-me-cta" class="animatedClick fast" title="Share this site" data-target="share-me-panel"><i class="fa fa-2x fa-share-alt btn-animate"></i></a>
                             <ul id="share-me" class="animated fadeInDown fast share-me-panel">
                                 <li><div class="fb-share-button" data-href="http://slamdunkdigital.com/" data-layout="button"></div></li>
                                 <li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://slamdunkdigital.com/" data-text="Slam Dunk Digital is a London digital collective specialising in web design and development." data-via="slamdunkdigital">Tweet</a></li>
                                 <li><script type="IN/Share" data-url="http://slamdunkdigital.com/"></script></li>
                             </ul>
                         </li>
+                        <li>
+                            <a href="http://instagram.com/slamdunkdigital" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
+                            <a href="http://facebook.com/slamdunkdigital" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+                            <a href="http://twitter.com/slamdunkdigital" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>                   
+                        </li>  
                     </ul>
                 </nav>
                 <div id="slogan" class="animatedParent animateOnce" data-sequence='750'>
